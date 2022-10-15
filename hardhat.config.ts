a10c97578199b88720815c6e186aa0ef0bd7434f
Rizky Ario <rizkyario@me.com>
2022-10-15 17:16:52 +0200

    Init project

---
+++ hardhat.config.ts
@@ -0,0 +1,20 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+    solidity: "0.8.17",
+    networks: {
+        goerli: {
+            url: process.env.STAGING_QUICKNODE_KEY,
+            accounts: [process.env.PRIVATE_KEY],
+        },
+        mainnet: {
+            url: process.env.PROD_QUICKNODE_KEY,
+            accounts: [process.env.PRIVATE_KEY],
+        },
+    },
+};
---
+++ scripts/deploy.ts
@@ -0,0 +1,31 @@
+import { ethers } from "hardhat";
+
+const main = async () => {
+  const nftContractFactory = await ethers.getContractFactory('MyEpicNFT');
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+
+  // Call the function.
+  let txn = await nftContract.makeAnEpicNFT()
+  // Wait for it to be mined.
+  await txn.wait()
+  console.log("Minted NFT #1")
+
+  txn = await nftContract.makeAnEpicNFT()
+  // Wait for it to be mined.
+  await txn.wait()
+  console.log("Minted NFT #2")
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
